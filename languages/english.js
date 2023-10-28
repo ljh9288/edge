52282a29149444741f51c8a47d9ab932c3dcdfe8
ljh9288 <ljh9288@naver.com>
2023-10-29 01:12:11 +0900

    lang

--- languages/english.js
+++ languages/english.js
@@ -1,3 +1,3 @@
-language.below_logo = "We are currently playing %%gamemode%% at %%map%%"
-language.loading = "Loading..."
-language.now_playing = "Now playing"
+language.below_logo = ""
+language.loading = ""
+language.now_playing = ""
