95c67364b8b353acf707c2898ee51542d9371092
ljh9288 <56786079+ljh9288@users.noreply.github.com>
2026-04-28 21:09:46 +0900

    logo
    dd

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ config.logo_use_image = true
 
 // Image filename for the server logo
 // (Place the image file in the images folder)
-config.logo_image = "lgo.png"
+config.logo_image = "logo.png"
 
 // Text to be shown on logo
 config.logo_text = "OVC"
